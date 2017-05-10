db21b63f429c81e0dbb2c2a2efdd50b350488ae6
lipgloss <gloss-water@gmail.com>
2017-05-10 11:12:32 -0600

    í ¾

--- src/components/main.js
+++ src/components/main.js
@@ -5,11 +5,11 @@ const Main = () => {
     return (
         <section className="section">
             <div className="container main">
-            	<PentaUrl speed='' to="https://lipgloss.sucks"/>
-            	<PentaUrl speed='1' to="https://lipgloss.sucks"/>
-            	<PentaUrl speed='2' to="https://lipgloss.sucks"/>
-            	<PentaUrl speed='3' to="https://lipgloss.sucks"/>
-            	<PentaUrl speed='4' to="https://lipgloss.sucks"/>
+                <PentaUrl speed='' to="https://soundcloud.com/lipglossed/" text="soundcloud" />
+            	<PentaUrl speed='1' to="https://twitter.com/glosstradamus" text="twitter"/>
+            	<PentaUrl speed='2' to="https://www.patreon.com/lipgloss" text="patreon" />
+            	<PentaUrl speed='3' to="https://github.com/gloss-water" text="github" />
+            	<PentaUrl speed='4' to="https://lipgloss.sucks" text="contact" />
             </div>
         </section>
     );
--- src/components/pentaurl.js
+++ src/components/pentaurl.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Penta from '../images/penta.svg';
 
-const PentaUrl = ({ speed, to }) => {
+const PentaUrl = ({ speed, text, to }) => {
     return (
-        <a href={to} className="box">
+        <a href={to} className={`box box${speed}`}>
             <img src={Penta} alt='pentacle' className={`penta${speed}`} />
+            <span>{text}</span>
         </a>
     );
 };
