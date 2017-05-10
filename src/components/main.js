175176711b7db76506455f65b5793bdf1fb9b2e3
lipgloss <gloss-water@gmail.com>
2017-05-10 00:08:43 -0600

    more wheels

--- src/components/main.js
+++ src/components/main.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import Penta from '../images/penta.svg';
+//import Penta2 from '../images/penta2.svg';
+//import Penta3 from '../images/penta2.svg';
+//import Penta4 from '../images/penta2.svg';
 
 const Main = () => {
     return (
         <section className="section">
-            <div className="container">
-                <div className="box" style={{ backgroundColor: '#C2DFE3', width: '15em', height: '15em' }}>
+            <div className="container main">
+                <a href="https://lipgloss.sucks" className="box">
                 	<img src={Penta} alt='pentacle' className="penta" />
-                </div>
+                </a>
+                <a href="https://lipgloss.sucks" className="box">
+                	<img src={Penta} alt='pentacle' className="penta2" />
+                </a>
+                <a href="https://lipgloss.sucks" className="box">
+                	<img src={Penta} alt='pentacle' className="penta3" />
+                </a>
+                <a href="https://lipgloss.sucks" className="box">
+                	<img src={Penta} alt='pentacle' className="penta4" />
+                </a>
             </div>
         </section>
     );
